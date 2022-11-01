2ed4163b5709e6a52268024aadb2d7384a62f456
Raghav Maheshwari <raghav.maheshwari@nagarro.com>
2022-11-01 10:56:14 +0530

    dependency conflicts resolved

--- src/app/_modules/shared.module.ts
+++ src/app/_modules/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
